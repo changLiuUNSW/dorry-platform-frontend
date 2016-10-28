c849848ce0820f873c88f1613166c31eda19d6ea
terry.li <terry.li@dorry.io>
2016-10-28 15:41:16 +0800

    modi scroll bar

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -1,4 +1,4 @@
 export class Constant {
-  public static DAEMONADDR = "http://192.168.10.84:5000";
+  public static DAEMONADDR = "http://localhost:5000";
   public static BUILDVERSION = "v0.1.2-alpha";
 }
