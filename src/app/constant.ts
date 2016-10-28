69da14075d43b195d993b94deb2d6142ba437748
terry.li <terry.li@dorry.io>
2016-10-28 16:05:57 +0800

    modi constant

--- src/app/constant.ts
+++ src/app/constant.ts
@@ -2,5 +2,4 @@ export class Constant {
   // public static DAEMONADDR = "http://192.168.10.84:5000";
   public static DAEMONADDR = 'http://localhost:5000';
   public static BUILDVERSION = "v0.1.4-alpha";
-
 }
