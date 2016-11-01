79f79cf8e119ea5ed45d07736eaf121fe05f7395
terry.li <terry.li@dorry.io>
2016-11-01 10:47:42 +0800

    foo

---
+++ dist/out-tsc/app/constant.js
@@ -0,0 +1,8 @@
+export var Constant = (function () {
+    function Constant() {
+    }
+    Constant.DAEMONADDR = 'http://localhost:5000';
+    Constant.BUILDVERSION = "v0.1.4-alpha";
+    return Constant;
+}());
+//# sourceMappingURL=../../../src/app/constant.js.map
