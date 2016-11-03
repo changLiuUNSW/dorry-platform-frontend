c8e768252067d370596e3e7d5377777cc1ade516
LiuBiHongHong <johnny.huang@dorry.io>
2016-11-03 11:35:53 +0800

    port changed

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo ..........Restarting the docker service on your machine..........
 sudo service docker stop
-sudo dockerd -H unix:///var/run/docker.sock -H tcp://0.0.0.0:5000 --api-enable-cors=true
+sudo dockerd -H unix:///var/run/docker.sock -H tcp://0.0.0.0:10000 --api-enable-cors=true
