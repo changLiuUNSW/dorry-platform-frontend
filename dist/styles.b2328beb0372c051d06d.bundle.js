83f45fa3a48ba36b5ca4bb4eb30d848907cdbad8
LiuBiHongHong <johnny.huang@dorry.io>
2016-12-02 19:09:25 +0800

    xxx

--- dist/styles.b2328beb0372c051d06d.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1,2],{443:function(n,o){},809:function(n,o,c){n.exports=c(443)}},[809]);
-//# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
--- src/app/market/market.ts
+++ src/app/market/market.ts
@@ -1,8 +1,9 @@
 export class Item {
   name: string;
   installing: boolean;
+  tags: any;
 
-  constructor(name: string, installing: boolean) {
+  constructor(name: string, installing: boolean, tags:any) {
     this.name = name;
     this.installing = installing;
   }
