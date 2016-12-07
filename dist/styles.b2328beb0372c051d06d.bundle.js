6643f6c1c3ad6fbfa51ca9f654ca07f12453e3ba
terry.li <terry.li@dorry.io>
2016-12-07 15:09:37 +0800

    rm dist

--- dist/styles.b2328beb0372c051d06d.bundle.js
+++ dist/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{443:function(n,o){},809:function(n,o,c){n.exports=c(443)}},[809]);
+webpackJsonp([1,2],{442:function(n,o){},809:function(n,o,c){n.exports=c(442)}},[809]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
