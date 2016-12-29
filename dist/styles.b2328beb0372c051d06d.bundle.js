19a23189972376da1d523a0ea3804e14505e3c36
terry.li <terry.li@dorry.io>
2016-12-29 16:21:14 +0800

    foo

--- dist/styles.b2328beb0372c051d06d.bundle.js
+++ dist/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{451:function(n,o){},763:function(n,o,c){n.exports=c(451)}},[763]);
+webpackJsonp([1,2],{452:function(n,o){},766:function(n,o,c){n.exports=c(452)}},[766]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
