3847f4fa1373b98d85b41dd25c5d46beac30a816
terry.li <terry.li@dorry.io>
2017-01-11 15:11:11 +0800

    build 0.1.15

--- dist/styles.b2328beb0372c051d06d.bundle.js
+++ dist/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,2],{455:function(n,o){},771:function(n,o,c){n.exports=c(455)}},[771]);
+webpackJsonp([1,2],{456:function(n,o){},772:function(n,o,c){n.exports=c(456)}},[772]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
