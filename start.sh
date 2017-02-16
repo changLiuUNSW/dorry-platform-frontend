feec898205d53e9127820c333c2d8796851051ac
terry.li <terry.li@dorry.io>
2017-02-16 09:45:58 +0800

    modi start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo ..........Restarting the docker service on your machine..........
-sudo service docker stop
-sudo dockerd -H unix:///var/run/docker.sock -H tcp://0.0.0.0:10000 --api-enable-cors=true
+echo .......... Run dorry-web docker image ..........
+sudo docker run -itd --name dorry-web --restart=always --privileged --network=host dorry-web
